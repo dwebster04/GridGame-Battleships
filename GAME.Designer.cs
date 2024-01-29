f5d94c0cb61d777220b346ef6c5506d22f2db8c9
dwebster04 <d.webster4034@gmail.com>
2024-01-29 18:44:17 +0000

    working on check if grid is valid, overlapping part not work

--- GAME.Designer.cs
+++ GAME.Designer.cs
@@ -9,10 +9,11 @@ namespace GridGame_Battleships
     partial class GAME
     {
         public Button[,] btn = new Button[7, 7];
+        public int[,] occupied = new int[7, 7];
         public ShipControl[,] ships;
 
         private System.ComponentModel.IContainer components = null;
-
+        private bool ValidShips = false;
         private Button btnCheck; // Declare the Submit button
 
         protected override void Dispose(bool disposing)
@@ -83,13 +84,24 @@ namespace GridGame_Battleships
             // Logic to check which buttons on the grid the ships are most on top of
             // You can iterate over the ships and compare their positions with the grid buttons
             // Perform the necessary logic here
-            Debug.WriteLine("Submit button clicked!");
+            Debug.WriteLine("Check button clicked!");
+            ValidShips = true;
+
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    occupied[i, j] = 0;
+                }
+            }
 
             for (int i = 0; i < 4; i++)
             {
+
                 Button btnClosest = new Button();
                 int distance = 0;
                 int closest = 1000000;
+                int closestX = 0; int closestY = 0;
                 for (int x = 0; x < 7; x++)
                 {
                     for (int y = 0; y < 7; y++)
@@ -102,12 +114,60 @@ namespace GridGame_Battleships
                         {
                             closest = distance;
                             btnClosest = btn[x, y];
+                            closestX = x; closestY = y;
                         }
                     }
 
-                   
+
                 }
                 ships[i, 0].Location = btnClosest.Location;
+                ShipControl checkShip = ships[i, 0];
+                if (checkShip.Height > checkShip.Width) // if ships is vertical
+                {
+                    // check to see if running off the grid
+                    int HeightValue = checkShip.Height / 50;
+                    if (closestY + HeightValue > 7)
+                    {
+                        Debug.Print("INVALID SHIP OF LENGTH: {0}", HeightValue);
+                        ValidShips = false;
+                    }
+
+                    // if occupied is greater than one ships will be overlapping
+                    for (int j = closestY; i < HeightValue; i++)
+                    {
+                        occupied[closestX, j] += 1;
+                    }
+
+                }
+                else // if ships is horizontal
+                {
+                    // check to see if running off the grid
+                    int WidthValue = checkShip.Width / 50;
+                    if (closestX + WidthValue > 7)
+                    {
+                        Debug.Print("INVALID SHIP OF LENGTH: {0}", WidthValue);
+                        ValidShips = false;
+                    }
+
+                    // if occupied is greater than one ships will be overlapping
+                    for (int j = closestX; i < WidthValue; i++)
+                    {
+                        occupied[j, closestY] += 1;
+                    }
+                }
+
+                for (int c = 0; c < 7; c++)
+                {
+                    for (int d = 0; d < 7; d++)
+                    {
+                        if (occupied[c, d] > 1)
+                        {
+                            ValidShips = false;
+                            Debug.Print("INVALID SHIPS ARE OVERLAPPING AT {0} , {1}", c , d);
+
+                        }
+                    }
+                }
             }
 }
 
